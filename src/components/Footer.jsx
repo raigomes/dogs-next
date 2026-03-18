e397f49de501c4edd264af4350b93c2e4439b4c8
raigomes <raigomes2@hotmail.com>
2026-03-18 00:05:11 -0300

    Add header and footer (without login)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import Image from "next/image";
+
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <Image src={"/assets/dog.svg"} alt="Dogs Logo" width={28} height={22} />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./Header.module.css";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link href="/" className={styles.logo}>
+          <Image
+            src={"/assets/dog.svg"}
+            alt="Dogs Logo"
+            width={28}
+            height={22}
+            priority={true}
+          />
+        </Link>
+
+        {/* {loggedIn && (
+          <Link to="/conta" className={styles.login}>
+            {user}
+          </Link>
+        )}
+
+        {!loggedIn && ( */}
+        <Link href="/login" className={styles.login}>
+          Login / Criar
+        </Link>
+        {/* )} */}
+      </nav>
+    </header>
+  );
 }
