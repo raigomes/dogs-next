14b87c48ca13e7305829c5b0ef3c1e097112974d
raigomes <raigomes2@hotmail.com>
2026-03-31 23:36:13 -0300

    Fix close modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,8 +8,17 @@ import style from "./Modal.module.css";
 export default function Modal({ children }: React.PropsWithChildren) {
   const router = useRouter();
 
+  function handleClose(e: React.MouseEvent<HTMLElement>) {
+    if (
+      e.target instanceof HTMLElement &&
+      e.target.classList.contains(style.modal)
+    ) {
+      router.back();
+    }
+  }
+
   return (
-    <div className={style.modal} onClick={() => router.back()}>
+    <div className={style.modal} onClick={handleClose}>
       {children}
     </div>
   );
